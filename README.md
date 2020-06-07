ae42c1ddc1f9a2463b5432bc4af07a0342d418b3
leonardovicentini <leonardovicentini5@gmail.com>
2020-06-07 17:54:38 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # vconv
+
+---
+
+Programma per convertiere i video mkv in mp4.
+
+### Requirements
+
+ffmpeg
+
+## Usage
+
+Il programma prevede i seguenti parametri:
+
+parametro | significato
+--- | ---
+--help | Restituisce il messaggio di help
+--version | Restituisce la versione del programma
+--verbose | Output verboso con le informazioni sulla conversione in corso.
+--quiet | Non restituisce alcun output sul terminale.
+--file FILE | Specifica il percorso del file mkv da convertire
+--dir DIR | Specifica il percorso di una cartella contenente i file mkv
+--out | Specifica una sottocartella del percorso indicato in cui verranno inseriti i file convertiti
+--test | Se presente imposta la conversione solo per i primi 30 secondi di video.
+
+Per ulteriori informazioni digitare:
+
+    $ vconv -h
+
+## Changelog
+
+**2020-04-11 02_01**
+
+Aggiunti i Flags --quiet, --verbose e --test.
+Risolto bug per cui il programma crashava se il file esisteva
+già nella cartella di destinazione
+
+**2020-04-11 01_01**
+
+Prima versione
+
+## Author
+
+Leonardo Vicentini
---
+++ bin/vconv.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "Vicentini Leonardo"
+__version__ = "01_01"
+
+
+import os
+import sys
+import argparse
+import ntpath
+import shutil
+
+
+boold = False
+
+
+def convert_file(file_path, output_folder):
+    """
+    """
+    global verbose, quiet, is_test
+
+    cwd = os.getcwd()
+
+    if not quiet and not verbose: print(f"Converting: {file_path} ...")
+
+    dir_path = os.path.dirname(os.path.abspath(file_path))
+    os.chdir(dir_path)
+    file_name = ntpath.basename(file_path).split(".")[0]
+
+    info = "" if verbose else "-loglevel quiet "
+    is_test = "-t 30 " if is_test else ""
+
+    os.system(f'ffmpeg {info}-i "{file_name}.mkv" {is_test}"{file_name}.mp4"')
+    
+    if not os.path.isdir(output_folder): os.mkdir(output_folder)
+
+    try:
+        if output_folder != ".": shutil.move(f"{file_name}.mp4", output_folder)
+    except shutil.Error:
+        print(f"File {file_name}.mp4 already exists")
+
+    os.chdir(cwd)
+
+
+if __name__ == "__main__":
+
+    if boold: print("Start main")
+
+    parser = argparse.ArgumentParser(description="Program to convert mkv into mp4.", prog="vconv")
+    parser.add_argument("--version", help="show program version", action="version", version=f"%(prog)s {__version__}")
+    parser.add_argument("-v", "--verbose", help="verbose output.", action="store_true")
+    parser.add_argument("-q", "--quiet", help="quiet output.", action="store_true")
+    parser.add_argument("-f", "--file", help="specify the mkv fil path.", type=str)
+    parser.add_argument("-d", "--dir", help="specify the path of the directory with mkv files.", type=str)
+    parser.add_argument("-o", "--out", help="specify the output folder.", type=str)
+    parser.add_argument("-t", "--test", help="convert only 30 sec of the video.", action="store_true")
+
+    args = parser.parse_args()
+
+    verbose = args.verbose
+    quiet = args.quiet
+    is_test = args.test
+
+    output_dir = args.out if args.out else "."
+
+    if args.file:
+        convert_file(args.file, output_dir) if os.path.isfile(args.file) else print("404 File not found. LOL :-)")
+
+    elif args.dir:
+        if os.path.isdir(args.dir):
+            for file in os.listdir(args.dir):
+                if file.endswith(".mkv"):
+                    file_name = file.split(".")[0]
+                    convert_file(f"{args.dir}/{file_name}.mkv", output_dir)
+        else:
+            print("404 Dir not found. LOL :-)")
+    
+    else:
+        parser.print_help()
+
+    if boold: print("End main")
